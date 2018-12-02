704597d42b470f45cb704097f656eac9cebf3ce6
EC2 Default User <ec2-user@ip-172-31-9-173.us-west-2.compute.internal>
2018-12-02 10:21:59 +0000

    final save to Models-Assignment

--- address_bloc/spec/address_book_spec.rb
+++ address_bloc/spec/address_book_spec.rb
@@ -18,14 +18,34 @@ require_relative '../models/address_book'
      end
    end
    
-  describe "#add_entry" do
+    describe "#remove_entry" do
+     it "removes only one entry, plus its sub-entries, from the address book" do
+      book = AddressBook.new
+      book.add_entry('Joke Silver', '281.555.4545', 'jsilver@myemail.com')
+      
+      name = 'Ada Lovelace'
+      phone_number = '010.012.1815'
+      email = 'augusta.king@lovelace.com'
+      book.add_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(2)
+
+      book.remove_entry(name, phone_number, email)
+
+      expect(book.entries.size).to eq(1)
+
+      expect(book.entries.first.name).to eq('Joke Silver')
+    end
+  end
+  
+    describe "#add_entry" do
      it "adds only one entry to the address book" do
        book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
  
        expect(book.entries.size).to eq(1)
      end
- 
+     
      it "adds the correct information to entries" do
        book = AddressBook.new
        book.add_entry('Ada Lovelace', '010.012.1815', 'augusta.king@lovelace.com')
